2a0fbb71154f6424bcd7edf6cc376b452e178e69
SarahTardif <tardif.sarah@courrier.uqam.ca>
2025-03-24 14:19:46 -0400

    updates

--- Contribution_Variables.r
+++ Contribution_Variables.r
@@ -5,18 +5,19 @@ library(caret)
 library(pdp)
 library(performanceEstimation)
 # Get variable importance measures
-model<-readRDS("modelRF_species_balanced_essentials_20250110.rds")
+model<-readRDS("modelRF_genus_balanced_20250110.rds")
 modrf<-model$finalModel
 imp = varImpPlot(modrf)
 # Normalisation en pourcentage
 imp <- as.data.frame(imp)
-imp$var <- rownames(imp)
 imp$MeanDecreaseAccuracy_Normalized <- (imp$MeanDecreaseAccuracy - min(imp$MeanDecreaseAccuracy)) / 
                                         (max(imp$MeanDecreaseAccuracy) - min(imp$MeanDecreaseAccuracy)) * 100
 
 imp$MeanDecreaseGini_Normalized <- (imp$MeanDecreaseGini - min(imp$MeanDecreaseGini)) / 
                                     (max(imp$MeanDecreaseGini) - min(imp$MeanDecreaseGini)) * 100
 # Création du premier boxplot pour MeanDecreaseAccuracy
+imp$var <- factor(imp$var, levels = imp$var[order(imp$MeanDecreaseAccuracy_Normalized, decreasing = TRUE)])
+
 ggplot(imp, aes(x =var, y = MeanDecreaseAccuracy_Normalized, fill = var)) +
   geom_point(size=4) +
   theme_minimal() +
@@ -24,6 +25,8 @@ ggplot(imp, aes(x =var, y = MeanDecreaseAccuracy_Normalized, fill = var)) +
     axis.text.y = element_text(size=30),
     legend.position = "none")
 # Création du deuxième boxplot pour MeanDecreaseGini
+imp$var <- factor(imp$var, levels = imp$var[order(imp$MeanDecreaseGini_Normalized, decreasing = TRUE)])
+
 ggplot(imp, aes(x =var, y = MeanDecreaseGini_Normalized, fill = var)) +
   geom_point(size=4) +
   theme_minimal() +
